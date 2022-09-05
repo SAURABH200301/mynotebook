569dbe9551f69e0aa3595b3175d62c9e1c8c9a76
SAURABH200301 <saurabhsharma200301@gmail.com>
2022-09-06 00:17:24 +0530

    created the new login point and new api

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -19,6 +19,7 @@ router.post('/createuser',
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
         }
+
         //check whether the user with same email exist
         try {
 
@@ -41,13 +42,51 @@ router.post('/createuser',
                 }
             }
             const authToken = jwt.sign(data, JWT_SECRET);
-            
-            res.json({authToken})
+
+            res.json({ authToken })
             // res.json(user);
         } catch (err) {
             console.log(err)
-            res.status(500).json("Some Error Occured");
+            res.status(500).send("Internal Server error Occured");
         }
     });
+
+
+//Create a User Using :POST "/api/auth/login" No login required
+router.post('/login',
+    [
+        body('email', 'Enter the valid mail id').isEmail(),
+        body('password', 'Password cannot be blank').exists()
+    ], async (req, res) => {
+        //if there are errors,return bad request and the errors
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        const { email, password } = req.body;
+        try{
+            let user= await User.findOne({email});
+            if(!user) 
+                return res.status(400).json({error: "Try correct credentials"});
+           
+            const passwordCompare = await bcrypt.compare(password,user.password);
+
+            if(!passwordCompare){
+                return res.status(400).json({error: "Try correct credentials"});
+            }
+
+            const data= {
+                user:{
+                    is: user.id
+                }
+            }
+            const authtoken=jwt.sign(data,JWT_SECRET);
+            res.json({authtoken});
+
+        }catch(error){
+            console.log(error)
+            res.status(500).json("Interal Server Error");
+        }
+    })
 
 module.exports = router;
